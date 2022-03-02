4a53e7109b7388d91e043d0da3ca0a966074d265
Averiser <averiser@gmail.com>
2022-03-02 14:15:49 +0300

    second commit - Question, Answers

--- QuizGame/GameViewController.swift
+++ QuizGame/GameViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class GameViewController: UIViewController {
+  
+  var gameModels = [Question]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupQuestions()
+        
     }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    configureUI()
+  }
+  
+  private func configureUI() {
     
+  }
+  
+  private func setupQuestions() {
+    gameModels.append(Question(text: "What is 2 + 2?", answers: [
+    Answer(text: "1", correct: false),
+    Answer(text: "2", correct: false),
+    Answer(text: "4", correct: true),
+    Answer(text: "7", correct: false)
+    ]))
+    
+    gameModels.append(Question(text: "What is 2 + 10?", answers: [
+    Answer(text: "1", correct: false),
+    Answer(text: "12", correct: true),
+    Answer(text: "4", correct: false),
+    Answer(text: "7", correct: false)
+    ]))
+    
+    gameModels.append(Question(text: "What is 2 + 0?", answers: [
+    Answer(text: "1", correct: false),
+    Answer(text: "5", correct: false),
+    Answer(text: "4", correct: false),
+    Answer(text: "2", correct: true)
+    ]))
+  }
+    
+  struct Question {
+    let text: String
+    
+    let answers: [Answer]
+  }
+  
+  struct Answer {
+    let text: String
+    let correct: Bool // true/false
+  }
 
 }
