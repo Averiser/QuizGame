2280fcbef9b62b702218177e658f066171eefc4d
Averiser <averiser@gmail.com>
2023-03-28 18:00:31 +0300

    first commit - Selection/deselection

--- QuizApp/SceneDelegate.swift
+++ QuizApp/SceneDelegate.swift
@@ -11,20 +11,25 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-//    guard let windowScene = (scene as? UIWindowScene) else { return }
-//    window?.windowScene = windowScene
-//    window?.makeKeyAndVisible()
-//    let controller = ViewController()
-//    let navController = UINavigationController(rootViewController: controller)
-//    window?.rootViewController = navController
-//
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+            window?.windowScene = windowScene
+    let viewController = QuestionViewController(question: "A question?", options: ["Option 1", "Option 2"]) { print($0) }
+    _ = viewController.view
+    viewController.tableView.allowsMultipleSelection = true
+    window?.rootViewController = viewController
+    window?.makeKeyAndVisible()
     
-          
-    guard let _ = (scene as? UIWindowScene) else { return }
-  }
+//        window?.windowScene = windowScene
+//        window?.makeKeyAndVisible()
+//        let controller = ViewController()
+//        let navController = UINavigationController(rootViewController: controller)
+//        window?.rootViewController = navController
+    
+    
+      }
 
   func sceneDidDisconnect(_ scene: UIScene) {
     // Called as the scene is being released by the system.
