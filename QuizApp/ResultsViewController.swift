2852d3d09f58950d7cab1f0f2ceff707f08740fe
Averiser <averiser@gmail.com>
2023-03-29 13:49:39 +0300

    2nd commit - adding Correct-,WrongAnswerCells

---
+++ QuizApp/ResultsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ResultsViewController.swift
+//  QuizGame
+//
+//  Created by MyMacBook on 29.03.2023.
+//
+
+import UIKit
+
+struct PresentableAnswer {
+  let question: String
+  let answer: String
+  let isCorrect: Bool
+}
+
+class CorrectAnswerCell: UITableViewCell {
+  @IBOutlet weak var answerLabel: UILabel!
+  @IBOutlet weak var questionLabel: UILabel!
+}
+
+class WrongAnswerCell: UITableViewCell {
+  @IBOutlet weak var questionLabel: UILabel!
+  @IBOutlet weak var correctAnswerLabel: UILabel!
+}
+
+class ResultsViewController: UIViewController, UITableViewDataSource   {
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var headerLabel: UILabel!
+  private var summary = ""
+  private var answers = [PresentableAnswer]()
+  
+  convenience init(summary: String, answers: [PresentableAnswer]) {
+    self.init()
+    self.summary = summary
+    self.answers = answers
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    headerLabel.text = summary
+    tableView.register(UINib(nibName: "CorrectAnswerCell", bundle: nil), forCellReuseIdentifier: "CorrectAnswerCell")
+    tableView.register(UINib(nibName: "WrongAnswerCell", bundle: nil), forCellReuseIdentifier: "WrongAnswerCell")
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return answers.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let answer = answers[indexPath.row]
+    if answer.isCorrect {
+      return correctAnswerCell(for: answer)
+    }
+    let cell = tableView.dequeueReusableCell(withIdentifier: "WrongAnswerCell") as! WrongAnswerCell
+    cell.questionLabel.text = answer.question
+    cell.correctAnswerLabel.text = answer.answer
+    return cell
+  }
+  
+  private func correctAnswerCell(for answer: PresentableAnswer) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CorrectAnswerCell") as! CorrectAnswerCell
+    cell.questionLabel.text = answer.question
+    cell.answerLabel.text = answer.answer
+    return cell
+  }
+  
+}
