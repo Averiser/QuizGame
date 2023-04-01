0bcffac40fad74f165c6f8e6ce66332ac3caaf2c
Averiser <averiser@gmail.com>
2023-04-01 17:31:47 +0300

    6th commit - navigControllerRouter

---
+++ QuizApp/NavigationControllerRouter.swift
@@ -0,0 +1,39 @@
+//
+//  NavigationControllerRouter.swift
+//  QuizGame
+//
+//  Created by MyMacBook on 31.03.2023.
+//
+
+import UIKit
+
+protocol ViewControllerFactory {
+  func questionViewController(for question: Question<String>, answerCallback: @escaping (String) -> Void) -> UIViewController
+  
+  func resultsViewController(for result: Result<Question<String>, String>) -> UIViewController
+}
+
+class NavigationControllerRouter: Router {
+  
+ private let navigationController: UINavigationController
+  private let factory: ViewControllerFactory
+  
+  init(navigationController: UINavigationController, factory: ViewControllerFactory) {
+    self.navigationController = navigationController
+    self.factory = factory
+  }
+  
+  func routeTo(question: Question<String>, answerCallback: @escaping (String) -> Void) {
+      show(factory.questionViewController(for: question, answerCallback: answerCallback))
+    }
+  
+  func routeTo(result: Result<Question<String>, String>) {
+    show(factory.resultsViewController(for: result))
+  }
+  
+  private func show(_ viewController: UIViewController) {
+    navigationController.pushViewController(viewController, animated: true)
+  }
+  
+  
+}
